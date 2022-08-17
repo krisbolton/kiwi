a3ac1f809de867aa4c0a0b648db51d8afede0db3
Kris Bolton <krisbolton@gmail.com>
2022-08-17 18:13:35 +0100

    add testing

---
+++ kiwi/tests/page/test_views.py
@@ -0,0 +1,11 @@
+from flask import url_for
+
+
+class TestPage(object):
+	def test_home_page_status(self, client):
+		response = client.get(url_for('page.home'))
+		assert response.status_code == 200
+
+	def test_privacy_page_status(self, client):
+		response = client.get(url_for('page.privacy'))
+		assert response.status_code == 200
