ab9d7b85d6fc826acf183225f8096f1ec95d9aa5
Kris Bolton <krisbolton@gmail.com>
2022-08-17 19:38:02 +0100

    test status of terms page

--- kiwi/tests/page/test_views.py
+++ kiwi/tests/page/test_views.py
@@ -9,3 +9,7 @@ class TestPage(object):
 	def test_privacy_page_status(self, client):
 		response = client.get(url_for('page.privacy'))
 		assert response.status_code == 200
+
+	def test_terms_page_status(self, client):
+		response = client.get(url_for('page.terms'))
+		assert response.status_code == 200
