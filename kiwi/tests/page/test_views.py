e8cddd3a07a7457830a378196c2d2d773066078e
Kris Bolton <krisbolton@gmail.com>
2022-08-19 17:56:04 +0100

    basic page status tests

--- kiwi/tests/page/test_views.py
+++ kiwi/tests/page/test_views.py
@@ -13,3 +13,15 @@ class TestPage(object):
 	def test_terms_page_status(self, client):
 		response = client.get(url_for('page.terms'))
 		assert response.status_code == 200
+
+	def test_courses_page_status(self, client):
+		response = client.get(url_for('page.courses'))
+		assert response.status_code == 200
+
+	def test_library_page_status(self, client):
+		response = client.get(url_for('page.library'))
+		assert response.status_code == 200
+
+	def test_community_page_status(self, client):
+		response = client.get(url_for('page.community'))
+		assert response.status_code == 200
